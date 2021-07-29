a214942b78405f79cca0f67657b127811e851e2f
Nikolay <freshitftm@gmail.com>
2021-07-29 10:50:05 +0300

    Add redux counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-function App() {
+import { connect } from 'react-redux';
+
+function App (props) {
+  const { step, count, dispatch } = props;
+  const increment = () => {
+    const action = { type: 'increment' };
+    dispatch(action);
+  };
+  const decrement = () => {
+    const action = { type: 'decrement' };
+    dispatch(action);
+  };
+  const onStepChange = ({ target: { value } }) => {
+    const action = { type: 'setStep', newStep: Number(value) };
+    dispatch(action);
+  };
   return (
     <div>
+      <h1>Current count: {count}</h1>
+      <input type='number' value={step} onChange={onStepChange} />
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => ({ count: state.count, step: state.step });
+
+/*
+const withProps = connect(mapStateToProps);
+const ComponentWithProps = withProps(App);
+export default ComponentWithProps;
+
+*/
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
+const initialState = {
+  count: 0,
+  step: 1,
+};
+
+function reducer (state = initialState, action) {
+  switch (action.type) {
+    case 'increment': {
+      return { ...state, count: state.count + state.step };
+    }
+    case 'decrement': {
+      return { ...state, count: state.count - state.step };
+    }
+    case 'setStep': {
+      return { ...state, step: action.newStep <= 0 ? 0.1 : action.newStep };
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+const store = createStore(reducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+/*
+  REDUX:
+  store - хранилище, js object
+  dispatch - функция, которая отправляет action в reducer
+  reducer - pure func; (state, action) => state
+  action - events. {type: string, ...bag}
+  REACT-REDUX:
+  connect - HOC. подписывает компонент на изменение store
+  mapStateToProps - func, что именно взять из store
+*/
